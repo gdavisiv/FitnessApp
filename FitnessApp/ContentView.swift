8f3b9c4efc7347aa58db216b27326bc9830881c3
George Davis IV <GDavisIV@gmail.com>
2020-07-22 09:44:04 -0400

    Xcode BETA 12: Building Fitness App UI

--- FitnessApp/ContentView.swift
+++ FitnessApp/ContentView.swift
@@ -9,12 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!").padding()
+        Home()
+            .preferredColorScheme(.dark)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Home: View {
+    var body: some View {
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack{
+                HStack{
+                    Text("Hello George")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                    
+                    Spacer(minLength: 0)
+                    
+                    Button(action: {}) {
+                        Image(systemName: "slider.horizontal.3")
+                            //.renderingMode(.original)
+                            .resizable()
+                            .frame(width: 35, height: 30)
+                            .foregroundColor(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
+                    }
+                }
+                .padding()
+            }
+        }
+        .background(Color.black.edgesIgnoringSafeArea(.all))
     }
 }
