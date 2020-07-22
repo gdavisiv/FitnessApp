0c1dbfc01b87553c2648b37a222fd40ad976b9d2
George Davis IV <GDavisIV@gmail.com>
2020-07-22 08:54:01 -0400

    Initial Commit

---
+++ FitnessApp/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  FitnessApp
+//
+//  Created by George Davis IV on 7/22/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by George Davis IV on 7/22/20.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
