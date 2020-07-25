ae0aa91863f019767b8ac2c161e346928b88f7b0
George Davis IV <GDavisIV@gmail.com>
2020-07-24 22:51:49 -0400

    Updates to UI

--- FitnessApp/ContentView.swift
+++ FitnessApp/ContentView.swift
@@ -47,15 +47,33 @@ struct Home: View {
                         .font(.system(size: 22))
                         .fontWeight(.bold)
                         .foregroundColor(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
+                    
+                    HStack(spacing: 15) {
+                        ForEach(workout_Data){ work in
+                            VStack{
+                                Text("2.5")
+                                    .foregroundColor(Color("Color"))
+                                
+                                Rectangle()
+                                    .fill(LinearGradient(gradient: .init(colors: [Color("Color"),Color("Color")]), startPoint: .top, endPoint: .bottom))
+                                    .frame(height: 200)
+                                
+                                Text(work.day)
+                                    .font(.caption)
+                                    .foregroundColor(.white)
+                            }
+                        }
+                    }
                 }
             }
         }
         .background(Color.black.edgesIgnoringSafeArea(.all))
+        //Enables Dark Mode
         .preferredColorScheme(.dark)
     }
 }
 
-//Sample Data..
+//Sample Data.. Struct + Array
 
 struct Daily : Identifiable {
     var id : Int
@@ -68,6 +86,8 @@ var workout_Data = [
     Daily(id: 0, day: "Day 1", workout_In_Min: 480),
     Daily(id: 1, day: "Day 2", workout_In_Min: 880),
     Daily(id: 2, day: "Day 3", workout_In_Min: 250),
-    Daily(id: 3, day: "Day 4", workout_In_Min: <#T##CGFloat#>)
-
+    Daily(id: 3, day: "Day 4", workout_In_Min: 360),
+    Daily(id: 3, day: "Day 5", workout_In_Min: 1220),
+    Daily(id: 3, day: "Day 6", workout_In_Min: 750),
+    Daily(id: 3, day: "Day 7", workout_In_Min: 950)
 ]
